ed379bffb4917b1d2962f579ac5be30ca37fadf5
abayoss <abayoss@yahoo.com>
2019-05-17 20:42:33 +0000

    require cors, user routes, mongoose useCreateIndex

--- app.js
+++ app.js
@@ -2,44 +2,45 @@ const express = require("express"),
   app = express(),
   port = 3000,
   postRouter = require("./routes/posts"),
+  userRouter = require("./routes/users"),
   mongoose = require("mongoose"),
   dbName = "maxNodeAng",
+  cors = require("cors"),
   bodyParser = require("body-parser");
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// grant access to the images
-app.use('/images', express.static('images'))
-
+// mongoose connection 
 mongoose
   .connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true })
   .then(console.log(`Connected to ${dbName}!`))
   .catch(err => console.log(err));
+// to silence the : collection.ensureIndex is deprecated
+mongoose.set("useCreateIndex", true);
 
-app.use((req, res, next) => {
-  // Website you wish to allow to connect || * to allow all websites
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,content-type,Accept,Authorization"
-  );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+// grant access to the storage folder 
+app.use("/images", express.static("images"))
 
-  // next() to pass control to the next middleware function 
-  next();
-});
+
+// enable CORS with various options
+let corsOption = {
+    "origin": ["http://127.0.0.1:5500","http://localhost:4200"],
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "allowedHeaders":"Origin,X-Requested-With,content-type,Accept,Authorization",
+    "preflightContinue": true,
+    "credentials":true
+}
+app.use(cors(corsOption));
 
 //Routes :
 app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
 
+// tests : nothing required, just a middleware i use to test packeges n stuff:
+const tests = require("./middleware/tests")
+app.get("/tests/:password", tests);
+
+// server 
 app.listen(port, () => console.log("server runing on port " + port + " ..."));
