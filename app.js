768249da34be4d56fdb100652ada7a1f77d173ce
abayoss <abayoss@yahoo.com>
2019-05-17 03:19:22 +0000

    allow access to multiple origins

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // grant access to the images
-app.use('/images', express.static('images'))
+app.use("/images", express.static("images"));
 
 mongoose
   .connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true })
@@ -20,7 +20,11 @@ mongoose
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect || * to allow all websites
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  const allowedOrigins = ["http://localhost:4200", "http://127.0.0.1:5500"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -35,7 +39,7 @@ app.use((req, res, next) => {
   // to the API (e.g. in case you use sessions)
   // res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // next() to pass control to the next middleware function 
+  // next() to pass control to the next middleware function
   next();
 });
 
