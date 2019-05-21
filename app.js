4fdb4b478f8372cda5aba2e833026e33c39b1e42
abayoss <abayoss@yahoo.com>
2019-05-21 03:08:20 +0100

    test

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use("/images", express.static("images"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// set view engine
+// set view engine 
 app.engine(
   "handlebars",
   exphbs({
